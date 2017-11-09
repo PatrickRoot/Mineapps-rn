410916c1bd97050f8d8035351df3cbe2d1fffdf9
Patrick <nianqinianyi@163.com>
2017-11-09 15:51:39 +0800

    Jpush

--- android/app/src/main/java/cn/sixlab/mineapps/MainApplication.java
+++ android/app/src/main/java/cn/sixlab/mineapps/MainApplication.java
@@ -3,7 +3,6 @@ package cn.sixlab.mineapps;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import cn.jpush.reactnativejpush.JPushPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -12,31 +11,38 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
+import cn.jpush.reactnativejpush.JPushPackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    // 设置为 true 将不弹出 toast
+    private boolean SHUTDOWN_TOAST = false;
+    // 设置为 true 将不打印 log
+    private boolean SHUTDOWN_LOG = false;
+
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage(),
+                    new JPushPackage(SHUTDOWN_TOAST, SHUTDOWN_LOG)
+            );
+        }
+    };
+
     @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
 
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new JPushPackage()
-      );
+    public void onCreate() {
+        super.onCreate();
+        SoLoader.init(this, /* native exopackage */ false);
     }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
 }
